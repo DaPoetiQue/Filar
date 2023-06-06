18d329d1301c101f80464f9d726255351d94f63a
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-06-06 23:10:11 +0200

    New Icons And Layout Adjustments Including Disabling Return Button.

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9598617e281a2ee76f1e1b15cb341bf03122ea46404de5d9b3bf14e6cf491d01
-size 811340
+oid sha256:f3490babea52795d616101bd7e21840e5a1d96c390205e9aa465d3959f5b9a02
+size 811745
