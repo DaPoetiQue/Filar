253f9bc13e18f21bad5e0f22abd516d15817a0d5
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-06-26 21:49:57 +0200

    Fixed Blur Content Layer

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47ee720d5e6db91c1dba4ef2af167bb3bd62db26746a4cf8a6bbd2571847cf89
-size 292040
+oid sha256:65825a298e3b78c995968ad13051472874d1dc43ee0a8a89a7d9d106d6fc66f6
+size 292501
