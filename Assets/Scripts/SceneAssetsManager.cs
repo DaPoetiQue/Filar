c2e1373cc32cd1c35083e05fc09b2e481df377ab
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-06-28 06:23:18 +0200

    Fixed Folder Widgets Loading.

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adb17e54fca0b6e94bdf9189f4b254caafd739f76eab889f66b8a5a7d7e2d96a
-size 295371
+oid sha256:874faf8607c49de07e184acce13ee28181a88f494a25b64cffc940affd4addbe
+size 305243
