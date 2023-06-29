777b1cebd7c05866de65e1c9132fa02e42da90c4
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-06-29 15:21:42 +0200

    Fixing Folder Creation

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:615dc80957ac89ec6fe39f8775c8b9ae9764c6ae422855810758af9230c8d982
-size 312198
+oid sha256:ef5a286de08ec8cb6e1e7098d9e9ab8b844fa5113952bd4fc8d34fa2a12ce317
+size 313886
