ad3a0436cb404d144ee4d90aadd632ea5dc54d55
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-06-30 09:01:29 +0200

    Fixed Project  Search Bar

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a17bc970dac2145a2f5480157e828dcd7a2105c3ff6848ad133732f586a7a7ce
-size 326829
+oid sha256:e9a1c3fa2c6c506faadd3525961cd843e8958fb75b7cf02db3d336610d3e78e5
+size 328760
