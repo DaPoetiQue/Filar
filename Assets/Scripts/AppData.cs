f11db4a94950dfce13821ab034ad15df4564a665
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-01 04:45:03 +0200

    Fixed Project Filter

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25e9eb536665b13cf2e480509d5d630e8f52f10878d7df73396ad5c8944f8c0f
-size 923461
+oid sha256:0a6818ff0f4a93db46e37b93d6218918ee1a0101e1041a8bb3dce13d3e104da9
+size 924086
