8462d4bf6bc60276711ad52f1414774cb2f9db31
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-07 19:59:18 +0200

    Added Dynamic Filter List - Work In Progress.

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13d296e2467bc841246c208257c962cdf91303876ff7ce8fffec44ac3abd9703
-size 393807
+oid sha256:c338fe5c80a7bd22811613e4b6a616f96c7bed9cd8100a56971d163fb00b47ad
+size 404960
