8b65d900f3bdc00e7568ec1c6e44fc8eb18d44e2
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-09 12:21:51 +0200

    Fixing Project Structure

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9678f93f21771082047ca3cb018f5861de69738accca432e0d74f8f07b92d5e
-size 972366
+oid sha256:8786ca4e3876a1ef3a652e785cc4d41df9fa035df9fff280ecd72c8302ef1c26
+size 975744
