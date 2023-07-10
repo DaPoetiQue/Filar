ad7dd36e0e4ee092f84d749b9b75be9b70df1b17
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-10 14:55:11 +0200

    Fixed Sorting List Partially

--- Assets/Scripts/DynamicWidgetsContainer.cs
+++ Assets/Scripts/DynamicWidgetsContainer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42f4f156510139b6ba7ceacd6f53b79a871ecd5e8a61088bcb1ea6c8cade2efb
-size 109513
+oid sha256:27d1502b40a84cfa7f9f7b515806307c35c0d52e044ff76cb4d1e828259629cc
+size 109508
