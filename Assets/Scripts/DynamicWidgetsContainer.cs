41c78f42575ce28e7f108bcd2d0e5ede1d064c4e
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-10 11:17:19 +0200

    Fixed Drop Down Hide On Deselection

--- Assets/Scripts/DynamicWidgetsContainer.cs
+++ Assets/Scripts/DynamicWidgetsContainer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03eb35f5f9d9191204f67478142c21694bcfbc58e99b6ddb03789c1d863c61c7
-size 109410
+oid sha256:42f4f156510139b6ba7ceacd6f53b79a871ecd5e8a61088bcb1ea6c8cade2efb
+size 109513
