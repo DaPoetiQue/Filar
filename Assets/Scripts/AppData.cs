0f152604b4d16f64e037fb1d8968b91492ac3bfa
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-11 18:03:31 +0200

    Added UI Screen Group Comonent

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cddf0b9d7805d847492d515a8788fe5e8fd90f47609bbf0ae7a0e7ac7022bb1d
-size 986914
+oid sha256:a9d8de3c1b50067677d78edfb86fc316cb1a9915f228f71e5349871e46818351
+size 988839
