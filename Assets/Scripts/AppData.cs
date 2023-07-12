4ff5bc3859a6bfd0991c7c9ab7d2e7e0fe5de4e0
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-12 08:38:22 +0200

    Fixed Project Creation Screen

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ea81a5d779cf7f3a23a1abb355b1f2a468d3cb3eed2a3204033fb79f601010c
-size 991316
+oid sha256:1df81ef15db66ab7db0013117e5b02810816c42046e485ca12c294c03658ea0b
+size 993499
