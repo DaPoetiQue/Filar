1e11102a9fa6301da110deda87389019c9563d86
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-12 10:41:35 +0200

    Fixed Filter Option Selection

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1df81ef15db66ab7db0013117e5b02810816c42046e485ca12c294c03658ea0b
-size 993499
+oid sha256:de63fc1ce4a9618502a429e50eb1149ae6637bb20f73a8fa59445e060dd903c0
+size 993935
