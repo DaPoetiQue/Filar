eef1224ab04f952cfede27b3383f1e9cef313451
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-14 05:11:35 +0200

    Fixed Selection Component To Work Dynamically

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cf66565ff77f9c0fe6e02f3ace411d2c321830467e33f2e936187b5a47e2454
-size 1013889
+oid sha256:eaef03203eba555587aec4b736403b5d5e520bc98c4528f594871093a845cbfa
+size 1007794
