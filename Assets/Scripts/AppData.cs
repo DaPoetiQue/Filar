0f7a2cebfafb7b8f695f19dde162f6eb281fd7ad
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-14 23:08:49 +0200

    Added Slection UI To Project View Screen

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c854ccdfb22c8f460df4584278d8e3d91b31c148560ff62d60fa1094ab30abc1
-size 1053892
+oid sha256:608ea3df189722d6f37215b1633418264d9d27b057afe5bfcfbd081c3988deff
+size 1054164
