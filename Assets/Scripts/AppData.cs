0cd6be9741ba4c1a3f30d0dbc256cdbe31d2a177
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-14 20:41:34 +0200

    Fixed Validations

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b23deec7c963fc7b26090db04ec35caf10e47d65fa7e4b8c9cc1dc3beb6ab49
-size 1052463
+oid sha256:c854ccdfb22c8f460df4584278d8e3d91b31c148560ff62d60fa1094ab30abc1
+size 1053892
