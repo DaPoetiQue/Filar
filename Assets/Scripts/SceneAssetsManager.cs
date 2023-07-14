e8da30fcb8b7474514bb26918eadac8a7bfa8671
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-14 23:08:49 +0200

    Added Slection UI To Project View Screen

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:218948740fbce270b4207eaeeb8db74a75691353dfa530f9ba7d441a89b74650
-size 429755
+oid sha256:ff2c4fc9d2ca4842779b0baeef103f26ad8ecdbb680544c45a44f1b05d114d50
+size 430033
