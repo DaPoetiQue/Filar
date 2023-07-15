232c687bc53410f5ad94ee4fce65d11fc0b176fe
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-15 17:27:54 +0200

    Fixed Input Field Disabled State Bug

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff2c4fc9d2ca4842779b0baeef103f26ad8ecdbb680544c45a44f1b05d114d50
-size 430033
+oid sha256:139da94b5d852733c5980535d2737344846aa16c909b3f8db4223c3b5b855022
+size 430029
