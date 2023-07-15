f86b92ecf1b2b96657496ebba521734a58d06052
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-15 08:19:06 +0200

    Fixed Broken UI Selection States And Added Data Debugger

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:608ea3df189722d6f37215b1633418264d9d27b057afe5bfcfbd081c3988deff
-size 1054164
+oid sha256:d06981e30da26baf79c007003c906eff6b1fe485253b6419d30b909245904ca7
+size 1049608
