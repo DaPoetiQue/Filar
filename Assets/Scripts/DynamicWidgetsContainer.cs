eb8448b394f86b606e422d5e58603fc4a6213480
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-16 09:21:53 +0200

    Fixing Scene Contents Loading

--- Assets/Scripts/DynamicWidgetsContainer.cs
+++ Assets/Scripts/DynamicWidgetsContainer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8ab0426d55f08a2a9d5dcb3228b98fc9ce6f2b05a4170dd08d315f3005438c0
-size 110637
+oid sha256:8d4fe8dc30fc37b1267164df728b34fdbe135a8f06da7a9574d59242c6b1de53
+size 111978
