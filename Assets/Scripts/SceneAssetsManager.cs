4af32130f592a7bcbd5c5bf27c6f5c8fe3570bbc
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-16 09:21:53 +0200

    Fixing Scene Contents Loading

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cb34fa5a64151fd1ea558010888a8d13e5657b5554a7e842f13d75adfea7e12
-size 437195
+oid sha256:e856b393f1fa0d9285d47eb0f47d62e7922d46e556147d3ccb1ba7c3229fedac
+size 426528
