fc64de7fb387fc239a4bf6007c63ece352ec331e
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-17 09:30:39 +0200

    Fixing Project View Screen Drop Down UI

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dff3cb608fa3f47a8c107cfb76dd00ec5264ad9d69488ad1b8700c119f71d49f
-size 432026
+oid sha256:15c3e807be3bc816313288849bf52cd949358011e7ff336b0dccb5beb483cf20
+size 432011
