8c01a8aada3a3deb04650865053769a733594438
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-17 07:38:06 +0200

    Fixed Empty Content Widget

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d326f7735ff3d30adac3ce7e14e678d1c97fec7cba809042bb4e555c07810c1
-size 427270
+oid sha256:dff3cb608fa3f47a8c107cfb76dd00ec5264ad9d69488ad1b8700c119f71d49f
+size 432026
