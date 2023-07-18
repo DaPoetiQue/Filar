b27b1349cda544a773dbb7a0457ab3add089f526
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-18 13:00:20 +0200

    Updating Storage Data For Scene Content Manager

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15c3e807be3bc816313288849bf52cd949358011e7ff336b0dccb5beb483cf20
-size 432011
+oid sha256:5621fe319653b8aebe6c291b26ddbffb77dce902952f4400a7cafa1d0c25472e
+size 434819
