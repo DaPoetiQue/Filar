1dd86e46b6cccc7853d4c18c7b4ad9c4702eed62
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-20 04:06:12 +0200

    Fixing Loading Sequence

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a83ee8dda012137547528cc794e393925e65b2124bdd18c35a429dd265bbfcf
-size 1073487
+oid sha256:8671b68bfa8839e39dc7acc38d7b8122bd59f0d8b9f16eba006be738a58c59b0
+size 1073670
