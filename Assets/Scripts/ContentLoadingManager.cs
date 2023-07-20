c00d6cbe748bd8c0e7693d787c6b1060d8064aad
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-20 05:06:59 +0200

    Added Content Loading Manager

---
+++ Assets/Scripts/ContentLoadingManager.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Com.RedicalGames.Filar
+{
+    public class ContentLoadingManager : AppMonoBaseClass
+    {
+        #region Static
+
+        private static ContentLoadingManager _instance;
+
+        public static ContentLoadingManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = FindObjectOfType<ContentLoadingManager>();
+
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Components
+
+        [SerializeField]
+        float tempLoadDuration = 3.0f;
+
+        #endregion
+
+        #region Main
+
+        public void LoadScreen(AppData.SceneDataPackets dataPackets)
+        {
+            StartCoroutine(loadScreenAsync(screenLoadedCallbackResults => 
+            {
+                if (screenLoadedCallbackResults.Success())
+                {
+                    AppData.Helpers.ComponentValid(ScreenUIManager.Instance, validComponentCallback => 
+                    {
+                        if (validComponentCallback.Success())
+                        {
+                            dataPackets.screenTransition = AppData.ScreenLoadTransitionType.Default;
+                            ScreenUIManager.Instance.ShowScreen(dataPackets);
+                        }
+                        else
+                            Log(validComponentCallback.resultsCode, "Screen UI Manager Is Not Yet Initialized.", this);
+                    });
+                }
+                else
+                    Log(screenLoadedCallbackResults.resultsCode, screenLoadedCallbackResults.results, this);
+            }));
+        }
+
+        IEnumerator loadScreenAsync(Action<AppData.Callback> callback)
+        {
+            AppData.Callback callbackResults = new AppData.Callback();
+
+            yield return new WaitForEndOfFrame();
+
+            var duration = tempLoadDuration;
+
+            while (duration > 0)
+            {
+                duration -= 1.0f * Time.deltaTime;
+                yield return null;
+            }
+
+            if(duration <= 0)
+            {
+                callbackResults.results = "Loading Completed.";
+                callbackResults.resultsCode = AppData.Helpers.SuccessCode;
+            }
+            else
+            {
+                callbackResults.results = "Loading Failed.";
+                callbackResults.resultsCode = AppData.Helpers.ErrorCode;
+            }
+
+            yield return new WaitForEndOfFrame();
+
+            callback.Invoke(callbackResults);
+
+        }
+
+
+        #endregion
+    }
+}
