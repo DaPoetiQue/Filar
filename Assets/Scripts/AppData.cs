61d4dc511b6169cfa8d135ae1f188c237b1f2bc1
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-20 09:46:14 +0200

    Updated Home View Screen Fader

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7de2c5a7059faea7e9d883979b769b3c655fe270224bef41e77ef1ac470c614c
-size 1078853
+oid sha256:d79a6947fa0ca7f8cb95b57db8dfede977d3d91853e89a41931b6f16bd47c628
+size 1080099
