e470764b7d304b9c78d27382bc4786bc649fce76
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-20 04:06:12 +0200

    Fixing Loading Sequence

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f72b4b69ce8f7ac7e43df72abb40c1c17e7f28849ef098d7d061131b3489eeeb
-size 442455
+oid sha256:0a30fec2c85c0b58597f3138d96814b03f0bca0d5e4b326caa5feea065eb31e9
+size 442262
