0342eb033a322ff5346f48a55578b524d81b5666
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-20 08:52:18 +0200

    Added Screen Fader Component

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f63e164549df15a3f5a1f546cb551b06c9f71a601599fb23b8aa2508cd67a59
-size 1077351
+oid sha256:7de2c5a7059faea7e9d883979b769b3c655fe270224bef41e77ef1ac470c614c
+size 1078853
