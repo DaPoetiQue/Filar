452fd542fa3ac6b6b3c46966537233170ec6b4d1
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-27 01:57:57 +0200

    Fixing Screen Initializations

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:708942c4a972b535812ea591c1faafb3d1de04eaeffa12b3bef3e908aad9ef21
-size 1082416
+oid sha256:8dc5b64bba8bc1788ae368307b202884c1ab541e4acb19e37545b41d5b9cbfb4
+size 1100815
