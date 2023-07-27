0f44c2409f3e7ee1ad3f07af96340e1c570b38e5
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-27 19:02:10 +0200

    Refactored Loading Screen

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d04842016d02ccd7b641fabff892e2516416005c7614557b31652fd0b966a98
-size 1099752
+oid sha256:21e3b972d49bde566976cf217d8bd951eb1d4fcd6b2e2b8e95ca93040a6a7a2c
+size 1101331
