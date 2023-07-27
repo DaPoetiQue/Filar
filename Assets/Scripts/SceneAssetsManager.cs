2859afba9af9c4e5ac0ad1f60fe5d5261e70f94a
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-27 06:34:42 +0200

    Setting Up App's Initial  Boot App Screen

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58ade0ceda3b0162ed25c180c49e8bd4599274332f9c55a530beffb0855889c7
-size 442292
+oid sha256:95a17692d5ec3ef679761df512d539c26947bf3c7da459ac710e9b51d2920756
+size 446749
