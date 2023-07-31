f38729d2ddef7e4900fb572c8eb082266b7b2800
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-07-31 19:53:50 +0200

    Refactored Go To Screen Function

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4ddb40cdc3135273506cbb85182811e158302918ad651cca63f98d29fa40bfe
-size 1108229
+oid sha256:b8404b4a46fd9fee3f0c119ba66899905656c7b6a1dbd3399cc00fbb62fba220
+size 1105741
