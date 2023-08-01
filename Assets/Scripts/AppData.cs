9733fb120cef8b3fbf035311234ba22fbc2078d5
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-08-01 05:24:29 +0200

    Added Message Displayer Widget

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f7b6998764a0dd5e52ba32ee799bcc6098ac15fec1c26f0fab7f2388c1c6f0e
-size 1114184
+oid sha256:f6a887e22eb3066bdb7c41958e625f59c0d117ec4f2442237176ebd5922743f9
+size 1114317
