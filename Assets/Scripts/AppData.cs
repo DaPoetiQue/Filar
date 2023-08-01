f50ccf29101c48d986b34c956548607202db1517
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-08-01 08:11:31 +0200

    Fixed Loading Tips

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e1b1b7395c432a04d7b7930f1e3486473f01dff628ffb90bc66f8d00bc5b458
-size 1114561
+oid sha256:2004ed8047b3141a09dca1f326ec299a9d9bed53915b077c52a7b3b845a6955c
+size 1114683
