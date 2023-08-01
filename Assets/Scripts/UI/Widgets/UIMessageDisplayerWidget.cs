588f1288bad4081a29463ec6962e025e60c93907
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-08-01 05:24:29 +0200

    Added Message Displayer Widget

---
+++ Assets/Scripts/UI/Widgets/UIMessageDisplayerWidget.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+namespace Com.RedicalGames.Filar
+{
+
+    public class UIMessageDisplayerWidget : AppData.Widget
+    {
+        #region Components
+
+        #endregion
+
+        #region Unity Callbacks
+        void Start() => Init();
+
+        #endregion
+
+        #region Main
+
+        new void Init()
+        {
+            messageDisplayerWidget = this;
+            base.Init();
+        }
+
+        protected override void OnHideScreenWidget()
+        {
+            HideSelectedLayout(AppData.WidgetLayoutViewType.DefaultView);
+        }
+
+        protected override void OnInputFieldValueChanged(string value, AppData.InputFieldDataPackets dataPackets)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnInputFieldValueChanged(int value, AppData.InputFieldDataPackets dataPackets)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnScreenWidget()
+        {
+
+        }
+
+        protected override void OnShowScreenWidget(AppData.SceneDataPackets dataPackets)
+        {
+            ShowSelectedLayout(AppData.WidgetLayoutViewType.DefaultView);
+        }
+
+        protected override void OnSubscribeToActionEvents(bool subscribe)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnScrollerValueChanged(Vector2 value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnCheckboxValueChanged(AppData.CheckboxInputActionType actionType, bool value, AppData.CheckboxDataPackets dataPackets)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnActionButtonEvent(AppData.WidgetType popUpType, AppData.InputActionButtonType actionType, AppData.SceneDataPackets dataPackets)
+        {
+
+        }
+
+        protected override void OnActionDropdownValueChanged(int value, AppData.DropdownDataPackets dataPackets)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
+}
