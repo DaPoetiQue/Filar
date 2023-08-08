f51be6f657661561e9e9081c2edf1f86ef348e37
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-08-08 19:43:06 +0200

    Initialization

---
+++ Assets/Scripts/UI/Widgets/SignInWidget.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+
+namespace Com.RedicalGames.Filar
+{
+    public class SignInWidget : AppData.Widget
+    {
+        #region Components
+
+        AppData.TransitionableUI transitionable;
+
+        bool isInitialView = true;
+
+        #endregion
+
+        #region Unity Callbacks
+        void Start() => Init();
+
+        #endregion
+
+        #region Main
+
+        new void Init()
+        {
+            signInWidget = this;
+
+            AppData.Helpers.GetAppComponentValid(SceneAssetsManager.Instance, SceneAssetsManager.Instance.name, sceneAssetsManagerCallbackResults => 
+            {
+                if (sceneAssetsManagerCallbackResults.Success())
+                {
+                    var widgetView = GetLayoutView(AppData.WidgetLayoutViewType.DefaultView);
+
+                    transitionable = new AppData.TransitionableUI(widgetContainer.value);
+                    transitionable.SetSpeed(sceneAssetsManagerCallbackResults.data.GetDefaultExecutionValue(AppData.RuntimeExecution.ScreenWidgetTransitionalSpeed).value);
+
+                    base.Init();
+                }
+                else
+                    Log(sceneAssetsManagerCallbackResults.resultCode, sceneAssetsManagerCallbackResults.result, this);
+            
+            }, "Scene Assets Manager Instance Is Not Yet Initialized.");
+        }
+
+        protected override void OnHideScreenWidget()
+        {
+            HideSelectedLayout(AppData.WidgetLayoutViewType.DefaultView);
+        }
+
+        protected override void OnInputFieldValueChanged(string value, AppData.InputFieldDataPackets dataPackets)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnInputFieldValueChanged(int value, AppData.InputFieldDataPackets dataPackets)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnScreenWidget()
+        {
+
+        }
+
+        protected override void OnShowScreenWidget(AppData.SceneDataPackets dataPackets)
+        {
+            LogInfo(" <============================================> Show Widget Now!", this);
+
+            ShowSelectedLayout(AppData.WidgetLayoutViewType.DefaultView);
+        }
+
+        protected override void OnSubscribeToActionEvents(bool subscribe)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnScrollerValueChanged(Vector2 value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnCheckboxValueChanged(AppData.CheckboxInputActionType actionType, bool value, AppData.CheckboxDataPackets dataPackets)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnActionButtonEvent(AppData.WidgetType popUpType, AppData.InputActionButtonType actionType, AppData.SceneDataPackets dataPackets)
+        {
+            if (actionType == AppData.InputActionButtonType.SignInViewChangeButton)
+            {
+                isInitialView = !isInitialView;
+
+                if (isInitialView)
+                    transitionable.SetTarget(widgetContainer.visibleScreenPoint);
+                else
+                    transitionable.SetTarget(widgetContainer.hiddenScreenPoint);
+
+                SwitchPage();
+            }
+        }
+
+        async void SwitchPage()
+        {
+
+            LogInfo(" <+++++++++++++> Go To Login Screen", this);
+
+            SetActionButtonState(AppData.InputActionButtonType.SignInViewChangeButton, AppData.InputUIState.Disabled);
+
+            await transitionable.TransitionAsync();
+
+            string buttonTitle = (isInitialView) ? "Sign In" : "Sign Up";
+
+            SetActionButtonTitle(AppData.InputActionButtonType.SignInViewChangeButton, buttonTitle);
+            SetActionButtonState(AppData.InputActionButtonType.SignInViewChangeButton, AppData.InputUIState.Enabled);
+
+            LogInfo(" <+++++++++++++> Went To Login Screen", this);
+        }
+
+        protected override void OnActionDropdownValueChanged(int value, AppData.DropdownDataPackets dataPackets)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void ScrollerPosition(Vector2 position)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+    }
+}
