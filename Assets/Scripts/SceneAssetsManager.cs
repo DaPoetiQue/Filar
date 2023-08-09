00547f288237079263bbab4dc8b2b655f56ffce9
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-08-09 09:22:35 +0200

    Refactoring Initialization

--- Assets/Scripts/SceneAssetsManager.cs
+++ Assets/Scripts/SceneAssetsManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ac1d2d9f85a264b2b71053c73b636cfc8fffaf82fd2f67a997bfdb4b245fb00
-size 458497
+oid sha256:b9bbe34845db783b8268b6efb2aba62ba1e136430ccf00c840f1a2b7b0c09654
+size 458501
