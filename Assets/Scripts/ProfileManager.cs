737c3645d67289d4f1723400b9210e42944dc3c0
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2023-08-11 08:35:50 +0200

    Clean Commit

--- Assets/Scripts/ProfileManager.cs
+++ Assets/Scripts/ProfileManager.cs
@@ -1,6 +1,6 @@
 using System;
-//using Firebase;
-//using Firebase.Auth;
+using Firebase;
+using Firebase.Auth;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -30,14 +30,14 @@ namespace Com.RedicalGames.Filar
         [Space(5)]
         public AppData.StorageDirectoryData profileStorageData = new AppData.StorageDirectoryData();
 
-        //[Space(5)]
-        //public DependencyStatus authDependencyStatus = DependencyStatus.UnavailableOther;
+        [Space(5)]
+        public DependencyStatus authDependencyStatus = DependencyStatus.UnavailableOther;
 
         public bool SignedIn { get; private set; }
 
         #region Firebase
 
-        //FirebaseAuth authentication;
+        FirebaseAuth authentication;
 
         public bool TermsAndConditionsAccepted { get; private set; }
 
@@ -56,20 +56,20 @@ namespace Com.RedicalGames.Filar
         void Setup()
         {
             //Initialize Authentication
-            //FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => 
-            //{
-            //    authDependencyStatus = task.Result;
+            FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+            {
+                authDependencyStatus = task.Result;
 
-            //    if (task.Exception == null)
-            //    {
-            //        if (authDependencyStatus == DependencyStatus.Available)
-            //            authentication = FirebaseAuth.DefaultInstance;
-            //        else
-            //            LogInfo($"Authentication Dependency Is Not Available - Dependency Status : {authDependencyStatus}", this);
-            //    }
-            //    else
-            //        throw task.Exception;
-            //});
+                if (task.Exception == null)
+                {
+                    if (authDependencyStatus == DependencyStatus.Available)
+                        authentication = FirebaseAuth.DefaultInstance;
+                    else
+                        LogInfo($"Authentication Dependency Is Not Available - Dependency Status : {authDependencyStatus}", this);
+                }
+                else
+                    throw task.Exception;
+            });
         }
 
         void CreateProfile(AppData.Profile profile, Action<AppData.CallbackData<AppData.Profile>> callback = null)
@@ -110,94 +110,94 @@ namespace Com.RedicalGames.Filar
             callback.Invoke(callbackResults);
         }
 
-        //public async Task<AuthError> SignInAsync(AppData.Profile profile)
-        //{
-        //    AuthError authError = AuthError.None;
+        public async Task<AuthError> SignInAsync(AppData.Profile profile)
+        {
+            AuthError authError = AuthError.None;
 
-        //    try
-        //    {
-        //        if (authDependencyStatus == DependencyStatus.Available)
-        //        {
-        //            await authentication.SignInWithEmailAndPasswordAsync(profile.GetUserEmail(), profile.GetUserPassword()).ContinueWith(signedInTaskCompletion =>
-        //            {
-        //                if (signedInTaskCompletion.Exception == null)
-        //                {
-        //                    if (signedInTaskCompletion.Result.User.IsValid())
-        //                    {
-        //                        LogSuccess($"User : {authentication.CurrentUser.DisplayName} Has Signed In Successfully.", this);
-        //                    }
-        //                    else
-        //                        LogWarning($"User : {profile.GetUserEmail()} Is Not Valid", this);
-        //                }
-        //                else
-        //                {
-        //                    FirebaseException fbException = signedInTaskCompletion.Exception.GetBaseException() as FirebaseException;
-        //                    authError = (AuthError)fbException.ErrorCode;
-        //                }
+            try
+            {
+                if (authDependencyStatus == DependencyStatus.Available)
+                {
+                    await authentication.SignInWithEmailAndPasswordAsync(profile.GetUserEmail(), profile.GetUserPassword()).ContinueWith(signedInTaskCompletion =>
+                    {
+                        if (signedInTaskCompletion.Exception == null)
+                        {
+                            if (signedInTaskCompletion.Result.User.IsValid())
+                            {
+                                LogSuccess($"User : {authentication.CurrentUser.DisplayName} Has Signed In Successfully.", this);
+                            }
+                            else
+                                LogWarning($"User : {profile.GetUserEmail()} Is Not Valid", this);
+                        }
+                        else
+                        {
+                            FirebaseException fbException = signedInTaskCompletion.Exception.GetBaseException() as FirebaseException;
+                            authError = (AuthError)fbException.ErrorCode;
+                        }
 
-        //                return authError;
-        //            });
-        //        }
+                        return authError;
+                    });
+                }
 
-        //        return authError;
-        //    }
-        //    catch (Exception exception)
-        //    {
-        //        FirebaseException fbException = exception.GetBaseException() as FirebaseException;
-        //        var errorCode = (AuthError)fbException.ErrorCode;
+                return authError;
+            }
+            catch (Exception exception)
+            {
+                FirebaseException fbException = exception.GetBaseException() as FirebaseException;
+                var errorCode = (AuthError)fbException.ErrorCode;
 
-        //        return errorCode;
-        //    }
-        //}
+                return errorCode;
+            }
+        }
 
-        //public async Task<AuthError> SignUpAsync(string userEmail, string userPassWord)
-        //{
-        //    AuthError authError = AuthError.None;
+        public async Task<AuthError> SignUpAsync(string userEmail, string userPassWord)
+        {
+            AuthError authError = AuthError.None;
 
-        //    try
-        //    {
-        //        if(authDependencyStatus == DependencyStatus.Available)
-        //        {
-        //            await authentication.CreateUserWithEmailAndPasswordAsync(userEmail, userPassWord).ContinueWith(async signedUpTaskCompletion => 
-        //            {
-        //                if (signedUpTaskCompletion.Exception == null)
-        //                {
-        //                    if (signedUpTaskCompletion.Result.User.IsValid())
-        //                    {
-        //                        LogSuccess($" <+++++++++++++++++++++> User : {userEmail} Has Been Created Successfully. Choose Varification Method.", this);
-        //                    }
-        //                    else
-        //                        LogWarning($"User : {userEmail} Is Not Valid", this);
-        //                }
-        //                else
-        //                {
-        //                    FirebaseException fbException = signedUpTaskCompletion.Exception.GetBaseException() as FirebaseException;
-        //                    authError = (AuthError)fbException.ErrorCode;
-        //                }
+            try
+            {
+                if (authDependencyStatus == DependencyStatus.Available)
+                {
+                    await authentication.CreateUserWithEmailAndPasswordAsync(userEmail, userPassWord).ContinueWith(async signedUpTaskCompletion =>
+                    {
+                        if (signedUpTaskCompletion.Exception == null)
+                        {
+                            if (signedUpTaskCompletion.Result.User.IsValid())
+                            {
+                                LogSuccess($" <+++++++++++++++++++++> User : {userEmail} Has Been Created Successfully. Choose Varification Method.", this);
+                            }
+                            else
+                                LogWarning($"User : {userEmail} Is Not Valid", this);
+                        }
+                        else
+                        {
+                            FirebaseException fbException = signedUpTaskCompletion.Exception.GetBaseException() as FirebaseException;
+                            authError = (AuthError)fbException.ErrorCode;
+                        }
 
-        //                return authError;
-        //            });
-        //        }
+                        return authError;
+                    });
+                }
 
-        //        return authError;
-        //    }
-        //    catch(Exception exception)
-        //    {
-        //        FirebaseException fbException = exception.GetBaseException() as FirebaseException;
-        //        var errorCode = (AuthError)fbException.ErrorCode;
+                return authError;
+            }
+            catch (Exception exception)
+            {
+                FirebaseException fbException = exception.GetBaseException() as FirebaseException;
+                var errorCode = (AuthError)fbException.ErrorCode;
 
-        //        return errorCode;
-        //    }
-        //}
+                return errorCode;
+            }
+        }
 
-        //public void SignOut()
-        //{
-        //    if (authDependencyStatus == DependencyStatus.Available)
-        //    {
-        //        LogInfo($"User : {authentication.CurrentUser.DisplayName} Has Signed Out.");
-        //        authentication.SignOut();
-        //    }
-        //}
+        public void SignOut()
+        {
+            if (authDependencyStatus == DependencyStatus.Available)
+            {
+                LogInfo($"User : {authentication.CurrentUser.DisplayName} Has Signed Out.");
+                authentication.SignOut();
+            }
+        }
 
         public void AcceptTermsAndConditions() => TermsAndConditionsAccepted = true;
 
